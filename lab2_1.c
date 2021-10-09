092ea59cc78efe0a89b8f3e3ceb3f66ee948e655
DyakovaM <91832009+DyakovaM@users.noreply.github.com>
2021-10-09 13:00:46 +0300

    Add files via upload

---
+++ lab2_1.c
@@ -0,0 +1,20 @@
+#inclue <stdio.h>
+#inclue <lostream>
+int f_factorial(int num)
+{if (a>0){
+	int a=1;
+	for (int i=1; i<=a; i++){
+		a=a*i;}
+		return a;
+	else{
+	return0;}
+}
+
+int main(int argc, char **argv)
+{
+int b=9;
+for (int i=1; i<=b; i++)
+	{printf(%i\n,i,f_factorial(i));
+	}
+	return(0);
+}
